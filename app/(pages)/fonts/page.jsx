977a747a82e347f626edd5c71162bb8da9ff67b5
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2025-02-21 17:55:41 -0700

    some stuff

---
+++ app/(pages)/fonts/page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const page = () => {
+	return (
+		<div>
+			<h1 className="text-black text-[5rem] Berlys">Lorem Ipsum Dolor Amet</h1>
+			<h1 className="text-black text-[5rem] CosmicVibrant">
+				Lorem Ipsum Dolor Amet
+			</h1>
+
+			<h1 className="text-black text-[5rem] Flazie">Lorem Ipsum Dolor Amet</h1>
+			<h1 className="text-black text-[5rem] MilkyWalky">
+				Lorem Ipsum Dolor Amet
+			</h1>
+			<h1 className="text-black text-[5rem] Baunk">Lorem Ipsum Dolor </h1>
+			<h1 className="text-black text-[5rem] Zighead">Lorem Ipsum Dolor Amet</h1>
+		</div>
+	);
+};
+
+export default page;
--- app/page.js
+++ app/page.js
@@ -1,101 +1,80 @@
+"use client";
 import Image from "next/image";
+import { FaPerson } from "react-icons/fa6";
+import { CiGrid41, CiBoxList } from "react-icons/ci";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+	const [isListView, setIsListView] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div>
+			<div className="flex flex-col bg-[#262627] min-h-screen">
+				{/* Header */}
+				<div className="flex flex-col md:flex-row justify-between items-start w-full mx-auto md:w-[93vw] mt-[3rem] md:mt-[2rem]">
+					<div className="flex flex-col text-center md:text-left gap-[0rem]">
+						<h1 className="text-[4.5rem] md:text-[8rem] leading-[5rem]  md:leading-[8rem] Flazie text-rose-300 glowing-text text-center md:text-left">
+							Eclipso
+						</h1>
+						<p className="text-neutral-100 text-[1.5rem] md:text-[1.75rem] leading-[2rem] Absans text-center md:text-left">
+							Eclipso {"-"} Align Your Habits, Unlock Your Potential.
+						</p>
+					</div>
+					<div className="flex flex-col text-center md:text-left gap-[0rem]">
+						<a
+							href="/profile"
+							className="text-neutral-200 flex gap-[6px] text-[1.5rem] items-center justify-center Absans transition-all pr-[0.5rem] hover:pr-0 hover:tracking-[0.25rem]"
+						>
+							<FaPerson size={26} />
+							Profile
+						</a>
+					</div>
+				</div>
+
+				{/* Boxes */}
+				<div className="flex w-[95vw] mx-auto h-[20vh] gap-[2px] mt-[2rem] rounded-md overflow-hidden">
+					<div className="w-full bg-neutral-700 h-[20vh] flex flex-col items-center justify-center">
+						<h1 className="text-[4vw] leading-[5vw] Baunk text-white">21</h1>
+						<p className="text-[1.75vw] leading-[2vw] text-white Absans">
+							Day Streak
+						</p>
+					</div>
+					<div className="w-full bg-neutral-700 h-[20vh] flex flex-col items-center justify-center">
+						<h1 className="text-[4vw] leading-[5vw] Baunk text-white">45</h1>
+						<p className="text-[1.75vw] leading-[2vw] text-white Absans">
+							Days Left
+						</p>
+					</div>
+					<div className="w-full bg-neutral-700 h-[20vh]"></div>
+					<div className="w-full bg-neutral-700 h-[20vh]"></div>
+					<div className="w-full bg-neutral-700 h-[20vh]"></div>
+				</div>
+				{/* Filters */}
+				<div className="flex flex-col md:flex-row justify-start items-start md:items-center w-[95vw] mx-auto gap-2 md:gap-6 mb- mt-8">
+					<div className="w-full">
+						<input
+							type="text"
+							placeholder="Search Articles..."
+							className="bg-neutral-900/30 border-2 border-neutral-900/10 ring-neutral-800 focus:ring-neutral-800 text-white px-8 py-3 rounded-full outline-none w-full text-[1.25rem]"
+						/>
+					</div>
+
+					<button className="bg-neutral-900/30 border-2 border-neutral-900/15 text-white px-6 py-3 rounded-full hidden md:flex">
+						{isListView ? <CiGrid41 size={30} /> : <CiBoxList size={30} />}
+					</button>
+				</div>
+				{/* Content */}
+				<div className="grid grid-cols-5 gap-y-[2rem] w-[95vw] mx-auto gap-[2rem] mt-[2rem]">
+					<div className="w-full bg-neutral-700 aspect-[1] rounded-md"></div>
+					<div className="w-full bg-neutral-700 aspect-[1] rounded-md"></div>
+					<div className="w-full bg-neutral-700 aspect-[1] rounded-md"></div>
+					<div className="w-full bg-neutral-700 aspect-[1] rounded-md"></div>
+					<div className="w-full bg-neutral-700 aspect-[1] rounded-md"></div>
+					<div className="w-full bg-neutral-700 aspect-[1] rounded-md"></div>
+					<div className="w-full bg-neutral-700 aspect-[1] rounded-md"></div>
+					<div className="w-full bg-neutral-700 aspect-[1] rounded-md"></div>
+				</div>
+			</div>
+		</div>
+	);
 }
